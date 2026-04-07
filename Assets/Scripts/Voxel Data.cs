0bce16b9106020f7e67a343f20ff51ab788b46db
Hiran S <hirans2006@gmail.com>
2026-04-07 13:10:45 +0530

    Add project files.

---
+++ Assets/Scripts/Voxel Data.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine;
+
+public static class VoxelData
+{
+    public readonly static Vector3[] voxelVerts = new Vector3[]
+    {
+        new(0,0,0),//0
+        new(1,0,0),//1
+        new(0,1,0),//2
+        new(1,1,0),//3
+        new(0,0,1),//4
+        new(1,0,1),//5
+        new(0,1,1),//6
+        new(1,1,1),//7
+    };
+    public readonly static int[,] voxelTris = new int[,]
+    {
+        {5,7,4,6},//Front
+        {0,2,1,3},//Back
+        {1,3,5,7},//Right
+        {4,6,0,2 },//Left
+        {2,6,3,7},//Top
+        {4,0,5,1},//Bottom
+    };
+}
